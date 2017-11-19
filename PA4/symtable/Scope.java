a98e59c4519aea56996c40b540f9657246f7b470
Matthew McGill <mhollismcgill@gmail.com>
2017-11-19 18:52:18 -0500

    lul

---
+++ PA4/symtable/Scope.java
@@ -0,0 +1,21 @@
+package symtable;
+import java.util.*;
+
+public class Scope {
+	//maps STE method or variable names to the STEs they enclose
+	private HashMap<String,STE> Enclosing;
+
+	public Scope() {
+		this.Enclosing = new HashMap<String,STE>();
+	}
+
+	public void setEnclosing(String Name, STE ste)
+    {
+    	this.Enclosing.put(Name, ste);
+    }
+    
+    public STE getEnclosing(String Name)
+    {
+    	return this.Enclosing.get(Name);
+    }
+}
